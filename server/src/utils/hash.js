039b6707878a27c30d02b2aac14b560997d9898b
Janek955 <jas.stawinski@gmail.com>
2026-08-07 00:03:23 +0200

    feat: add node-gyp-build package with necessary files and scripts
    - Added LICENSE, README.md, SECURITY.md, and package.json for node-gyp-build.
    - Implemented bin.js and build-test.js for build and testing functionalities.
    - Created index.js and node-gyp-build.js for module loading and prebuild handling.
    - Added optional.js for handling optional native dependencies.
    - Introduced controllers, middleware, repositories, routes, and services for auth and game functionalities.

---
+++ server/src/utils/hash.js
@@ -0,0 +1,12 @@
+import bcrypt from "bcrypt";
+
+
+const SALT_ROUNDS = 12;
+
+export async function hashPassword(password) {
+    return await bcrypt.hash(password, SALT_ROUNDS);
+}
+
+export async function comaprePassword(password, hash) {
+    return await bcrypt.compare(password, hash);
+}
