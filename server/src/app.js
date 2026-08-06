886e3dd2e008c3dc7eb189075e7d64f6f1e25397
Janek955 <jas.stawinski@gmail.com>
2026-08-06 18:34:10 +0200

    feat: update database connection to use DATABASE_URL and implement user retrieval endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import pool from "./database/db.js";
 
 const app = express();
 
@@ -8,6 +9,16 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Serwer działa!");
+});
+
+app.get("/users", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM users");
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json( { error: "Błąd servera " });
+    }
 });
 
 export default app;
