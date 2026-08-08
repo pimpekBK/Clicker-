b2769928528841c8f0ea65fa9bb5b608f1f077ff
pimpekBK <p.balcerzak10@gmail.com>
2026-08-08 19:41:22 +0200

    naprawa mobka  / nidokończone

--- frontend/src/clicker/Clicker.jsx
+++ frontend/src/clicker/Clicker.jsx
@@ -2,35 +2,20 @@ import { useState } from "react";
 import styles from "./Clicker.module.css";
 import Mob1 from "../mobs/Mob1";
 
-
-
 export default function Clicker(){
     const[count, setcount] = useState(0);
-    const[normalIF, setNormal] = useState(true);
-    const timeoutRef = useRef();
-    
+    const[isHitting, setIsHitting] = useState(true);
 
     function Setcount(){
-        setcount(count=> count + 1);
-        
-        Hit();
+        setcount(count => count + 1);
+        setIsHitting(false)
     }
-    function Hit(){
-        clearTimeout(timeoutRef.current);
-        setNormal(false);
-        console.log(normalIF);dd
-        setTimeout(() =>
-            {
-                setNormal(true);
-                console.log(normalIF);
-            },200);
-        
-    }
+
     return(
         <>
         <button className={styles.container} onClick={Setcount}></button>
         <h3 className={styles.number}>{count}</h3>
-        <Mob1 normal={normalIF}/>
+        <Mob1 hitting={count} ></Mob1>
         </>
     );
 }
--- frontend/src/mobs/Mob1.jsx
+++ frontend/src/mobs/Mob1.jsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
-import Mob1Normal from "../assets/mob1-normal.png";
-import Mob1Angry from "../assets/mob1-angry.png";
+import { useState, useEffect } from "react";
+import MobNeutral from "../assets/mob1-neutral.png";
+import MobHiting from '../assets/mob1-hiting.png'
 
-export default function Mob1(normal){
+export default function Mob1({count}){
+    const[hitting, sethitting] = useState(false)
+    
+    useEffect(()=> {
+        sethitting(true)
+          
+        const timer = setTimeout(() => {
+            sethitting(false)
+        }, 200);
 
+        return () => clearTimeout(timer);
+          
+    },[count])
+    
     return(
         <>
-        {normal &&(
-            <img src={Mob1Normal} className="Mob1Normal" width="600" height="auto" ></img>
-        )}
-        {!normal &&(
-            <img src={Mob1Angry} className="Mob1Normal" width="600" height="auto" ></img>
-        )}
+        {/* {console.log('Mob1')}; */}
+        {console.log(hitting)};
+        {hitting === true ? <img src={MobNeutral} className="MobNeutral" width="600" height="auto" ></img> : <img src={MobHiting} className="MobHiting" width="600" height="auto" ></img>} ;
         </>
     );
 }
